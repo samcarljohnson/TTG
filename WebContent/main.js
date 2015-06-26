e1ab237abc83b88d9a3d12a6765fa15cf68663c9
Sam Johnson <samcarljohnson@gmail.com>
2015-06-26 09:32:50 -0400

    main.js posting correct data, and doPost firing correctly

--- WebContent/main.js
+++ WebContent/main.js
@@ -5,6 +5,21 @@
 $(document).ready(function(){
   $("form").submit(function(event){
     console.log("Prevented default submit.");
+    
+    var frm = $("form");
+    var data = JSON.stringify(frm.serializeArray());
+    console.log(data);
+    
+    var postData = $.ajax({
+      method: "POST",
+      url: "http://localhost:8080/TTG/login",
+      data: data
+    });
+    
+    postData.done(function(msg){
+      alert("finished ::"+msg);
+    });
+    
     event.preventDefault();
   });
 });
